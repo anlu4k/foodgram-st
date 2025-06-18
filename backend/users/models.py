fcab2fc4c9b51d1c9a9f0872d240c075b780b9e5
Mov <152440560+MoVa53@users.noreply.github.com>
2025-06-18 20:05:33 +0300

    Add model user

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.db.models import UniqueConstraint
 
-# Create your models here.
+
+class User(AbstractUser):
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = [
+        'username',
+        'first_name',
+        'last_name',
+    ]
+    email = models.EmailField(
+        'Электронная почта',
+        max_length=254,
+        unique=True,
+    )
+
+    class Meta:
+        ordering = ['id']
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+    def __str__(self):
+        return self.username
